b16c2afe7e67b5df3b6ce3eac94e9ad42bfc1770
Daniel Mikes <max8@post.cz>
2021-07-29 21:47:30 +0200

    secret

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+const aws = require('aws-sdk');
+
+// let s3 = new aws.S3({
+//   accessKeyId: process.env.S3_KEY,
+//   secretAccessKey: process.env.S3_SECRET
+// });
+
 let secret;
 try {
   secret = (process.env.MONGO_URI || secrets.MONGO_URI);
