831accba817c8f745a292570f363b0117c241680
Daniel Mikes <max8@post.cz>
2021-07-29 20:31:58 +0200

    correction

--- myApp.js
+++ myApp.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const log = (...args) => console.log(...args)
-
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
