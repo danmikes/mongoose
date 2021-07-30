7620624a3181b0d5b8ff1987b08435459506bfc9
Daniel Mikes <max8@post.cz>
2021-07-30 09:52:18 +0200

    chain

--- myApp.js
+++ myApp.js
@@ -97,7 +97,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person.find({ favoriteFoods: foodToSearch })
-    .sort({ name: 1 })
+    .sort({ name: -1 })
     .limit(2)
     .select()
     .exec((err, foundPeople) => {
